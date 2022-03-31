23f8a70a3e70725ad721148a434558d0ddd43f6a
josephjlyons <josephjlyons90@gmail.com>
2022-03-31 13:21:30 -0400

    install methodoverride for tricking form to a put req not post

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const req = require('express/lib/request');
+
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -23,6 +25,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 
 app.get('/', (req, res) => {
@@ -49,6 +52,16 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { campground });
 });
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+     res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.listen(3000, () => {
     console.log('On port 3000')
