6b9a0833a585f45c99f60fc9c1309c202922d451
josephjlyons <josephjlyons90@gmail.com>
2022-04-12 14:42:06 -0400

    delete campground deletes all reviews for campground

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const { stringify } = require('nodemon/lib/utils');
+const Review = require('./review');
+const { campgroundSchema } = require('../schemas');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -15,5 +16,15 @@ const CampgroundSchema = new Schema({
         }
     ]
 });
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if(doc){
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema)
