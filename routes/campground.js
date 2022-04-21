5bc07dd881e32b9f12da1708b1e2c44cb4773cce
josephjlyons <josephjlyons90@gmail.com>
2022-04-21 15:07:30 -0400

    combine same route paths to clean up file more

--- routes/campground.js
+++ routes/campground.js
@@ -6,19 +6,20 @@ const Campground = require('../models/campground');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCamp))
 
-router.get('/', catchAsync(campgrounds.index));
+router.get('/new', isLoggedIn, campgrounds.newCampForm);
 
-router.get('/new', isLoggedIn,campgrounds.newCampForm);
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCamp))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCamp))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCamp))
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCamp));
 
-router.get('/:id', catchAsync(campgrounds.showCamp));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.editCampForm));
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCamp));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCamp));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,13 +5,17 @@ const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const users = require('../controllers/users');
 
-router.get('/register', users.registerForm);
 
-router.post('/register', catchAsync(users.registerUser));
+router.route('/register')
+    .get(users.registerForm)
+    .post(catchAsync(users.registerUser));
 
-router.get('/login', users.loginForm);
+router.route('/login')
+    .get(users.loginForm)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+
+
 
 router.get('/logout', users.logout)
 
