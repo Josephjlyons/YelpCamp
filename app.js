34cb03d473bf8874bef32c8d66125d62193b0673
josephjlyons <josephjlyons90@gmail.com>
2022-05-03 14:13:33 -0400

    install mongoSanitize | protect mongoDB from inject

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const mongoSanitize = require('express-mongo-sanitize');
 const ExpressError = require('./utils/ExpressError');
 const User = require('./models/user');
-
 const userRoutes = require('./routes/user');
 const campgroundRoutes = require('./routes/campground');
 const reviewRoutes = require('./routes/reviews');
@@ -41,6 +41,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
 
 const sessionConfig = {
     secret: 'SecretShhh',
